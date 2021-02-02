fe98bc7c097df096b931fc1f779193d245a0d5ab
happy149 <hosnearahappybd@gmail.com>
2021-02-02 16:20:06 +0600

    nullVSundefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,32 @@
+// let pakhi;
+
+/* function add(num1,num2){
+    console.log(num1+num2);
+    return
+
+}
+const result = add (13,82);
+console.log(result); */
+
+ /*function add(num1,num2){
+    console.log(num1,num2);
+    return
+
+}
+const result = add (13);
+console.log(result); */
+
+/*function add(num1,num2){
+    console.log(num1,num2);
+
+}
+const result = add (13);
+console.log(result); */
+
+/* const pamik = { name: "smart dudi", phone:234567};
+console.log(pamik.gf); */
+
+/*let  fun = undefined;
+console.log (fun); */
+let ages = [2,5,9]
+console.log(ages[11]);
