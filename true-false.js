99f03c8f402facfb3d2ed55a93130eb7b80f0f7a
happy149 <hosnearahappybd@gmail.com>
2021-02-02 12:08:42 +0600

    true-flase

---
+++ true-false.js
@@ -0,0 +1,55 @@
+// number
+
+/*const age = 4
+
+
+if (age) {
+    console.log("condition is true");
+}
+else{
+    console.log ("condition is false");
+}
+
+// string
+const name = ""
+
+
+if (name) {
+    console.log("condition is true");
+}
+else{
+    console.log ("condition is false");
+} */
+
+// undifuned
+
+/* let name;
+console.log(name);
+
+if (name) {
+    console.log("condition is true");
+}
+else{
+    console.log ("condition is false");
+} */
+// false condition pabo :
+// 0
+// ""
+// undefined
+// null
+// NaN
+// = false
+
+//Truthy condition pabo :
+// '0',' ', [], "false"
+
+let name = 12;
+
+if (name || name  == 0 ) {
+    console.log("condition is true");
+}
+else{
+    console.log ("condition is false");
+}
+
+
