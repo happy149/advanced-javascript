e0814526f13164a5d26dcc0f9bea975720842de8
happy149 <hosnearahappybd@gmail.com>
2021-02-02 18:59:29 +0600

    double-vs-triple-equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,57 @@
+/*const first = 2;
+const second = "2";
+if (first ==second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is False")
+} */
+
+/*const first = 2;
+const second = "2";
+if (first ===second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is False") */
+
+   /* const first = 2;
+const second = true;
+if (first ==second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+} */
+//true ar value 1 so condition is false
+/* const first = 1;
+const second = true;
+if (first ==second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+} 
+// true value is 1.so condition is true
+*/
+
+/*const first = 0;
+const second = false;
+if (first ==second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+} 
+// false value is 0.so condition is true
+*/
+
+const first = 0;
+const second = false;
+if (first === second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+} 
+// false value is 0.but type is bulian. === so condition is false;
