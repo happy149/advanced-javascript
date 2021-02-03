13d068b6b1b468b0705adf5e75874e2cf398920e
happy149 <hosnearahappybd@gmail.com>
2021-02-03 18:55:29 +0600

    break-continue

---
+++ break-continue.js
@@ -0,0 +1,35 @@
+// original for loop
+/*
+const nums = [1,2,3,4,5,6,7,8,9]
+for(let i = 0; i < nums.length; i++){
+    console.log(nums[i]); 
+} */
+
+//jokhon break korta chai
+
+/* const nums = [1,2,3,4,5,6,7,8,9]
+for(let i = 0; i < nums.length; i++){
+    console.log(nums[i]); 
+    if(nums[i]>6){
+        break;
+    }
+} */
+
+//deferent continue (negitive asba nah)
+/* const nums = [1,2,-3,4,5,-6,7,8,9]
+for(let i = 0; i < nums.length; i++){
+    if(nums[i]< 0 ){
+        continue;
+    }
+    console.log(nums[i]); 
+} */
+
+//jodi nagetive soho chai, continue nah leklae hoba
+
+/* const nums = [1,2,-3,4,5,-6,7,8,9]
+for(let i = 0; i < nums.length; i++){
+    if(nums[i]< 0 ){
+        
+    }
+    console.log(nums[i]); 
+} */
