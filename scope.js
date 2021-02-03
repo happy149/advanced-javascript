418409ec322e7a8eee3be2287daea60d3a70174f
happy149 <hosnearahappybd@gmail.com>
2021-02-03 13:17:57 +0600

    letConstant

---
+++ scope.js
@@ -0,0 +1,135 @@
+
+// jokhon normal vaba koro
+
+/*function sum (first , second){
+    let result = first + second;
+    return result;
+}
+
+const output = sum (3,7);
+console.log(output); */
+// jokhon bira thaka korbo error dakhava
+
+/*function sum (first , second){
+    let result = first + second;
+    return result;
+}
+
+const output = sum (3,7);
+console.log(result);
+console.log(output); */
+
+
+// jokhon vitor thaka console.log(result); korbo
+/* function sum (first , second){
+    let result = first + second;
+    console.log(result);
+    return result;
+}
+
+const output = sum (3,7);
+console.log(output); */
+
+// bonus k  global hisava vitor bira sob jiga thaka call kora ji
+/*
+ let bonus = 20;
+function sum (first , second){
+    let result = first + second;
+    console.log(bonus);
+    return result;
+}
+
+const output = sum (3,7);
+console.log(bonus);
+console.log(output); */
+
+//jokhon vitor ra consol.log koraci
+
+/* function sum (first , second){
+    let result = first + second;
+    if (result >9) {
+        const mood ="happy"
+        console.log(mood);
+    }
+    return result;
+}
+
+const output = sum (3,7);
+console.log(output); */
+
+//jokhon let or const variable deya vitor bira koraci error dakhaba
+
+/*function sum (first , second){
+    let result = first + second;
+    if (result >9) {
+        let mood ="happy"
+           mood = "fancy"
+           mood = "orocy"
+        console.log(mood);
+    }
+    console.log(mood);
+    return result;
+    
+    
+}
+const output = sum (3,7); */
+
+// var deya korla dakhaba
+
+/*function sum (first , second){
+    let result = first + second;
+    if (result >9) {
+        var mood ="happy"
+           mood = "fancy"
+           mood = "orocy"
+        console.log(mood);
+    }
+    console.log(mood);
+    return result;
+      
+}
+const output = sum (3,7); */
+
+// var dela sudu vaira thaka o kora
+
+/*function sum (first , second){
+    let result = first + second;
+    if (result >9) {
+        var mood ="happy"
+           mood = "fancy"
+           mood = "orocy"
+        //console.log(mood);
+    }
+    console.log(mood);
+    return result;
+      
+}
+const output = sum (3,7);
+*/
+//var de declair korlam
+/*
+function sum (first , second){
+    let result = first + second;
+    if (result >9) {
+        var mood ="happy"
+           mood = "fancy"
+           mood = "orocy"
+        //console.log(mood);
+    }
+    var day = " friday"
+    console.log(day);
+    return result;
+      
+}
+const output = sum (3,7);
+*/
+
+// var deya  aga consol kori undefined dakaba
+// console.log(day);
+// var day = " friday"
+
+
+
+// let or const deya korla error dakhaba
+console.log(day);
+let day = " friday"
